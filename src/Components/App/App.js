5b402f0a2f11d17a989ad818fd6a85d262c0728c
Amber Shipley <ashipley2@me.com>
2023-04-19 09:27:27 -0700

    Create after check in Success card component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,15 @@
+import { Container, Col, Row, Button } from 'react-bootstrap';
 import Home from '../Home/Home';
 import Success from '../Success/Success';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap';
 import Header from '../Header/Header';
-import Row from 'react-bootstrap/Row';
 import './App.css';
 
 function App() {
   return (
     <main className="App">
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <Success />
     </main>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,6 @@ export default function Header() {
         <Container className='d-flex justify-content-between mt-4'>
           <h1>Where's Sunny</h1>
           <p>You last checked in on April 2, 2023</p>
-          <Button variant="outline-info">See how you measure up!</Button>
         </Container>
       </header>
   );

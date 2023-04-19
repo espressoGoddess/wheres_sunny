5a569415db746928d93a5da32ba6747160d593af
Amber Shipley <ashipley2@me.com>
2023-04-18 18:48:53 -0700

    Set up basic Home component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,20 @@
 import Home from '../Home/Home';
 import Success from '../Success/Success';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import { Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
 import './App.css';
 
 function App() {
   return (
     <main className="App">
       <header className="App-header">
-        <h1>Where's Sunny</h1>
-        <h2>You last checked in on April 2, 2023</h2>
+        <Container className='d-flex justify-content-between mt-4'>
+          <h1>Where's Sunny</h1>
+          <p>You last checked in on April 2, 2023</p>
+
+        </Container>
       </header>
       <Home />
     </main>

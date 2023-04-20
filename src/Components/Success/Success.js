2e64c0b3665255a1b6bf5cbdb981225c1e9c704e
Amber Shipley <ashipley2@me.com>
2023-04-20 10:26:04 -0700

    Update Success component to dynamically add content from currentLog

--- src/Components/Success/Success.js
+++ src/Components/Success/Success.js
@@ -1,25 +1,42 @@
 import './Success.css';
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { DateTime } from 'luxon';
 import { Button, Card, Col, Row, Container } from 'react-bootstrap';
 
-export default function Success({ log }) {
+export default function Success({ log, setLogs }) {
+
+  useEffect(() => {
+    setLogs(JSON.parse(localStorage.getItem('user1_checkin')));
+  }, [])
+
+  const formattedDate = () => {
+    if (log) {
+      return DateTime.fromFormat(log.date, 'yyyy-MM-dd').toLocaleString(DateTime.DATE_MED);
+    }
+  }
+
   const history = useHistory();
   const routeToStats = () => {
     history.push('/see-your-points');
   }
   return (
+    log ? 
     <Container>
       <Row>
         <Col md={{span: 6, offset: 3}}>
           <Card className='text-center mt-5' border='light'>
-            <Card.Header className='text-start'>April 20, 2023</Card.Header>
+            <Card.Header className='text-start'>{formattedDate()}</Card.Header>
             <Card.Body>
               <Card.Title className='mt-4 fs-1'>🌞</Card.Title>
               <Card.Subtitle className='mt-3'>
-                Yay! The current weather in Bellingham is sunny!
+                Success, You checked in!
               </Card.Subtitle>
               <Card.Text className='mt-2'>
-                You got 2 points! You now have 14 total points.
+                The current weather in {log.location.city}, {log.location.state} is {log.weather_condition}.
+                </Card.Text>
+              <Card.Text className='mt-2'>
+                You got {log.pointsReceived} points. You now have  total points.
               </Card.Text>
               <Button variant='outline-info' onClick={routeToStats}>See how you measure up!</Button>
             </Card.Body>
@@ -27,6 +44,7 @@ export default function Success({ log }) {
         </Col>
       </Row>
     </Container>
+    : null
   );
 }
 

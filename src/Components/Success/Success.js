3ad9fee45bfa8cdf225d43c4055dc6b4497ba82f
Amber Shipley <ashipley2@me.com>
2023-04-22 12:07:45 -0700

    Invoke function after declaration instead of in jsx

--- src/Components/Success/Success.js
+++ src/Components/Success/Success.js
@@ -16,11 +16,11 @@ export default function Success({ log }) {
     setLogs(JSON.parse(localStorage.getItem('user1_checkin')));
   }, [])
 
-  const formattedDate = () => {
+  const formattedDate = (() => {
     if (log) {
       return DateTime.fromFormat(log.date, 'yyyy-MM-dd').toLocaleString(DateTime.DATE_MED);
     }
-  }
+  })();
 
   const points = (() => {
     if (!logs) {
@@ -38,7 +38,7 @@ export default function Success({ log }) {
       <Row>
         <Col md={{span: 6, offset: 3}}>
           <Card className='text-center mt-5' border='light'>
-            <Card.Header className='text-start'>{formattedDate()}</Card.Header>
+            <Card.Header className='text-start'>{formattedDate}</Card.Header>
             <Card.Body>
               <Card.Title className='mt-4 fs-1'>{log.icon}</Card.Title>
               <Card.Subtitle className='mt-3'>

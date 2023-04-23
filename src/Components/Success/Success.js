b09c11da6b931603e31cede5c38f41a00bd4e797
Amber Shipley <ashipley2@me.com>
2023-04-23 12:13:29 -0700

    Reorganize content in Success component

--- src/Components/Success/Success.js
+++ src/Components/Success/Success.js
@@ -17,7 +17,7 @@ export default function Success({ log, forecast }) {
 
   const formattedDate = (() => {
     if (log) {
-      return DateTime.fromFormat(log.date, 'yyyy-MM-dd').toLocaleString(DateTime.DATE_MED);
+      return DateTime.fromFormat(log.date, 'yyyy-MM-dd').toLocaleString({ month: 'long', day: 'numeric' });
     }
   })();
 
@@ -30,39 +30,56 @@ export default function Success({ log, forecast }) {
       return acc;
     }, 0);
   })();
-
+  
   const nextSunnyDay = (() => {
     if (!forecast) {
       return;
     }
     const forecastedSunnyIndex = forecast.findIndex(data => data === 'sunny');
     if (forecastedSunnyIndex >= 0) {
-      return `Good news, it should be sunny in ${forecastedSunnyIndex + 1} days`;
+      return `It should be sunny in ${forecastedSunnyIndex + 1} days!`;
     } return `It doesn't look like it's going to be sunny for the next week 😔`;
   })();
 
   return (
     log ? 
-    <Container>
+    <Container  className='mt-6'>
       <Row>
-        <Col md={{span: 6, offset: 3}}>
-          <Card className='text-center mt-5'>
-            <Card.Header className='d-flex align-items-center justify-content-between'><div>{formattedDate}</div><div>successful check in</div></Card.Header>
-            <Card.Body>
-              <Card.Title className='mt-4 fs-1'>{log.icon}</Card.Title>
-              <Card.Text className='mt-2'>
-                The current weather in {log.location.city}, {log.location.state} is {log.weather_condition}.
+        <Col md={{span: 4, offset:1}}>
+          <Card border='light' className='text-center'>
+            <Card.Title className='mt-5'>{formattedDate}</Card.Title>
+              <Card.Body>
+                <Card.Text className='mt-2 fs-0'>
+                  {log.icon}
                 </Card.Text>
-              <Card.Text className='mt-2'>
-                You got {log.pointsReceived} point{log.pointsReceived > 1 ? 's' : null}. You now have {points} total points.
+                <Card.Text className='mt-2 mb-3 fs-5'>
+                  {log.weather_condition}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+        </Col>
+        <Col md={{span: 4, offset: 2}}>
+          <Card className='text-center'>
+            <Card.Header>
+             {log.pointsReceived > 1 ? 'You got' : 'You only got'}
+            </Card.Header>
+            <Card.Body>
+              <Card.Text className=' fs-1 mb-0'>
+                {log.pointsReceived}
+              </Card.Text>
+              <Card.Text className='mt-0'>
+                point{log.pointsReceived > 1 ? 's' : null}
+              </Card.Text>
+              <Card.Text>
               </Card.Text>
               <Button variant='outline-info' onClick={routeToStats}>See my points</Button>
+              <Card.Text className='mt-2'>
+                {nextSunnyDay.includes('should') ? <p>Good News:</p> : null}   
+                <p>{nextSunnyDay}</p>
+              </Card.Text>
             </Card.Body>
-              {forecast ? (<Card.Footer className='mt-2 text-start'>
-                {nextSunnyDay}
-              </Card.Footer>) : null}
           </Card>
-          <footer className='mt-3'>
+          <footer className='mt-3 text-end'>
           <p>powered by <a className=" link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover" href="https://www.weatherapi.com/" title="Weather API">WeatherAPI</a></p>
         </footer>
         </Col>
